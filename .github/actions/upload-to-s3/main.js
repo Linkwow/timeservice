a46009b68f459d7cfd55a747ead0640e304ac146
Tungsten <linkwow75@gmail.com>
2025-08-29 09:34:13 +0300

    added upload to s3 workflow

---
+++ .github/actions/upload-to-s3/main.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+function run() {
+    let bucket = core.getInput('bucket', {required: true});
+    let region = core.getInput('region', {required: true});
+    let folder = core.getInput('folder', {required: true});
+
+    exec.exec('ls')
+    exec.exec(`aws s3 sync ${folder} s3://${bucket} --region ${region}`);
+}
+
+run();
+
+
