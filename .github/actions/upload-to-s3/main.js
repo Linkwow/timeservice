dd5944e446a617b630a262fb68cfdb8221685052
Tungsten <linkwow75@gmail.com>
2025-09-01 08:44:38 +0300

    Added outputs

--- .github/actions/upload-to-s3/main.js
+++ .github/actions/upload-to-s3/main.js
@@ -3,12 +3,15 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
-    let bucket = core.getInput('bucket', {required: true});
-    let region = core.getInput('region', {required: true});
-    let folder = core.getInput('folder', {required: true});
+    let bucket = core.getInput('bucket', {required: true})
+    let region = core.getInput('region', {required: true})
+    let folder = core.getInput('folder', {required: true})
 
     exec.exec('ls ./target/')
-    exec.exec(`aws s3 sync ${folder} s3://${bucket} --region ${region}`);
+    exec.exec(`aws s3 sync ${folder} s3://${bucket} --region ${region}`)
+
+    let s3Url = `http://${bucket}.s3-website-us-east-1.amazonaws.com/`
+    core.setOutput('bucket-url', s3Url)
 }
 
 run();
